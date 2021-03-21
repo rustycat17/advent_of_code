67eb45940446944990b339d0f94976409a31e9e6
rustycat17 <rustycat17@posteo.nz>
2021-03-21 17:35:15 +0100

    added improvement for second question of 2015-12-01

--- 2015/d01/src/main.rs
+++ 2015/d01/src/main.rs
@@ -3,19 +3,23 @@ use std::fs;
 fn main() {
     // let input = "(())";
 
-    let input = fs::read_to_string("data/input.txt")
-        .expect("unable to read file data/input.txt");
+    let input = fs::read_to_string("data/input.txt").expect("unable to read file data/input.txt");
 
     let mut floor = 0;
+    let mut first_basement = 0;
 
-    for char in input.chars() {
+    for (index, char) in input.chars().enumerate() {
         match char {
             '(' => floor += 1,
             ')' => floor -= 1,
             _ => panic!("wrong symbol {}", char),
         }
-        println!("current floor: {}", floor);
+
+        if floor == -1 && first_basement == 0 {
+            first_basement = index + 1;
+        }
     }
 
+    println!("first time in the first basement: {}", first_basement);
     println!("final floor: {}", floor);
 }
