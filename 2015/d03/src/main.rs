7aff6baf606530d366307e41b20fa1b81d2f11fe
rustycat17 <rustycat17@posteo.nz>
2021-03-23 22:50:41 +0100

    refactoring by extracting parse_direction() function

--- 2015/d03/src/main.rs
+++ 2015/d03/src/main.rs
@@ -1,8 +1,6 @@
 use std::fs;
 
-#[derive(Debug)]
-#[derive(Copy, Clone)]
-#[derive(PartialEq)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 struct Point {
     x: i32,
     y: i32,
@@ -26,58 +24,40 @@ fn main() {
         Err(err) => panic!("could not open file {}, error message {}", filename, err),
     };
 
-    let mut coord1 = Point {
-        x: 0,
-        y: 0,
-    };
-    let mut coord2 = Point {
-        x: 0,
-        y: 0,
-    };
+    let mut coord1 = Point { x: 0, y: 0 };
+    let mut coord2 = Point { x: 0, y: 0 };
 
     let mut unique_locations = vec![coord1];
 
     for (index, symbol) in input.chars().enumerate() {
         // println!("{}", symbol);
         if (index + 1) % 2 != 0 {
-            if symbol == '>' {
-                coord1 = coord1.calc_coordinates(Point {x: 1, y: 0});
-            } else if symbol == '<' {
-                coord1 = coord1.calc_coordinates(Point {x: -1, y: 0});
-            } else if symbol == '^' {
-                coord1 = coord1.calc_coordinates(Point {x: 0, y: 1});
-            } else if symbol == 'v' {
-                coord1 = coord1.calc_coordinates(Point {x: 0, y: -1});
-            } else {
-                panic!("unknown symbol: {}", symbol);
-            }
+            coord1 = parse_direction(coord1, symbol);
             if !unique_locations.contains(&coord1) {
                 unique_locations.push(coord1);
             }
         } else {
-            if symbol == '>' {
-                coord2 = coord2.calc_coordinates(Point {x: 1, y: 0});
-            } else if symbol == '<' {
-                coord2 = coord2.calc_coordinates(Point {x: -1, y: 0});
-            } else if symbol == '^' {
-                coord2 = coord2.calc_coordinates(Point {x: 0, y: 1});
-            } else if symbol == 'v' {
-                coord2 = coord2.calc_coordinates(Point {x: 0, y: -1});
-            } else {
-                panic!("unknown symbol: {}", symbol);
-            }
+            coord2 = parse_direction(coord2, symbol);
             if !unique_locations.contains(&coord2) {
                 unique_locations.push(coord2);
             }
         }
-        println!("symbol: {:?}", symbol);
-        println!("index+1: {}", index + 1);
-        println!("point 1: {:?}", coord1);
-        println!("point 2: {:?}", coord2);
-
     }
 
-    // println!("{:?}", coord);
-    println!("{:?}", unique_locations);
     println!("{}", unique_locations.len());
+}
+
+fn parse_direction(mut coord: Point, symbol: char) -> Point {
+    if symbol == '>' {
+        coord = coord.calc_coordinates(Point { x: 1, y: 0 });
+    } else if symbol == '<' {
+        coord = coord.calc_coordinates(Point { x: -1, y: 0 });
+    } else if symbol == '^' {
+        coord = coord.calc_coordinates(Point { x: 0, y: 1 });
+    } else if symbol == 'v' {
+        coord = coord.calc_coordinates(Point { x: 0, y: -1 });
+    } else {
+        panic!("unknown symbol: {}", symbol);
+    }
+    coord
 }
