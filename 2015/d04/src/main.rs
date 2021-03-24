78cf0423d2d8fbab339e560ded0039aed2ef8344
rustycat17 <rustycat17@posteo.nz>
2021-03-24 23:51:39 +0100

    added implementation for 2015-12-04

--- 2015/d04/src/main.rs
+++ 2015/d04/src/main.rs
@@ -1,3 +1,22 @@
+use crypto::md5::Md5;
+use crypto::digest::Digest;
+
 fn main() {
-    println!("Hello, world!");
+    let secret_key = String::from("bgvyzdsv");
+
+    let mut counter = 1;
+    loop {
+        let teststr = secret_key.clone() + &counter.to_string();
+        counter += 1;
+
+        let mut hash = Md5::new();
+        hash.input_str(&teststr);
+
+        if hash.result_str().starts_with("000000") {
+            println!("{}", teststr);
+            break;
+        }
+
+    }
+
 }
