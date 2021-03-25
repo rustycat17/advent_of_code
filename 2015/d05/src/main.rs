2a7a676b8c290abb11bae7cd4f4a8c7231538866
rustycat17 <rustycat17@posteo.nz>
2021-03-25 12:02:42 +0100

    refactored code

--- 2015/d05/src/main.rs
+++ 2015/d05/src/main.rs
@@ -8,43 +8,42 @@ fn main() {
         Err(err) => panic!("could not open file, error message: {}", err),
     };
 
-    let mut count_good_strings = 0;
+    let mut good_strings = 0;
     for line in input.split('\n') {
-        let mut vowel_counts = 0;
-
-        let mut last_letter = '_';
-        let mut double_letter = false;
-
-        let mut bi_string = false;
-
-        for character in line.chars() {
-            match character {
-                'a' | 'e' | 'i' | 'o' | 'u' => vowel_counts += 1,
-                _ => (),
-            }
-
-            if !bi_string {
-                bi_string = match (last_letter, character) {
-                    ('a', 'b') => true,
-                    ('c', 'd') => true,
-                    ('p', 'q') => true,
-                    ('x', 'y') => true,
-                    _ => false,
-                };
-            }
-
-            if !double_letter {
-                double_letter = character == last_letter;
-            }
-
-            last_letter = character;
-        }
-
-        if vowel_counts >= 3 && double_letter && !bi_string {
-            count_good_strings += 1;
-        }
+        good_strings = calculate_good_strings(good_strings, line)
     }
 
-    println!("{}", count_good_strings);
+    println!("{}", good_strings);
+}
 
+fn calculate_good_strings(mut good_strings: i32, line: &str) -> i32 {
+    let mut vowel_counts = 0;
+
+    let mut last_letter = '_';
+    let mut double_letter = false;
+
+    let mut bi_string = false;
+
+    for character in line.chars() {
+        match character {
+            'a' | 'e' | 'i' | 'o' | 'u' => vowel_counts += 1,
+            _ => (),
+        }
+
+        if !bi_string {
+            bi_string = matches!((last_letter, character), ('a', 'b') | ('c', 'd') | ('p', 'q') | ('x', 'y'));
+        }
+
+        if !double_letter {
+            double_letter = character == last_letter;
+        }
+
+        last_letter = character;
+    }
+
+    if vowel_counts >= 3 && double_letter && !bi_string {
+        good_strings += 1;
+    }
+
+    good_strings
 }
