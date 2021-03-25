301c623df098132dca00a1e4746056eacf4a6231
rustycat17 <rustycat17@posteo.nz>
2021-03-25 11:54:55 +0100

    implemented first version of first task for day 5

--- 2015/d05/src/main.rs
+++ 2015/d05/src/main.rs
@@ -1,3 +1,50 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let filepath = "data/input.txt";
+    let input = fs::read_to_string(filepath);
+    let input = match input {
+        Ok(input) => input,
+        Err(err) => panic!("could not open file, error message: {}", err),
+    };
+
+    let mut count_good_strings = 0;
+    for line in input.split('\n') {
+        let mut vowel_counts = 0;
+
+        let mut last_letter = '_';
+        let mut double_letter = false;
+
+        let mut bi_string = false;
+
+        for character in line.chars() {
+            match character {
+                'a' | 'e' | 'i' | 'o' | 'u' => vowel_counts += 1,
+                _ => (),
+            }
+
+            if !bi_string {
+                bi_string = match (last_letter, character) {
+                    ('a', 'b') => true,
+                    ('c', 'd') => true,
+                    ('p', 'q') => true,
+                    ('x', 'y') => true,
+                    _ => false,
+                };
+            }
+
+            if !double_letter {
+                double_letter = character == last_letter;
+            }
+
+            last_letter = character;
+        }
+
+        if vowel_counts >= 3 && double_letter && !bi_string {
+            count_good_strings += 1;
+        }
+    }
+
+    println!("{}", count_good_strings);
+
 }
